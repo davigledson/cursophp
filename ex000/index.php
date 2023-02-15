9095e754b02a6cde0f82e7f788c675c73a1c94e8
davigledson <davigledson@outlook.com>
2023-02-15 02:49:44 -0300

    Inicio e Hello World em PHP
    Hello World = me livraste da maldição nesse repositório também  👌

--- ex000/index.php
+++ ex000/index.php
@@ -1,12 +1,21 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Me livrando da maldição</title>
 </head>
 <body>
-    
+    <!--tem ponto e virgula no final de todo comando-->
+    <!--Echo ou print = tanto faz-->
+    <h1>
+        <?php 
+        echo "Olá, Mundo! \u{1F30E}"; 
+        
+        
+        ?>
+    </h1>
+    <p>Vamos tenta  nos livrar da maldição</p>
 </body>
 </html>
---
+++ ex001_DadosdoServidor/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dados do Servidor</title>
+</head>
+<body>
+    <h1>Dados do Servidor</h1>
+    <?php 
+   // phpinfo(); Evitar dados sensíveis serem vazados
+    
+    ?>
+</body>
+</html>
